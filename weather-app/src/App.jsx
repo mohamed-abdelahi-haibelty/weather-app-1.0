d2eeb49870c24e13aa22db57a2c588e7d84a178b
mohamed-abdelahi-haibelty <96340736+mohamed-abdelahi-haibelty@users.noreply.github.com>
2023-04-27 13:32:50 +0000

    fetch current weather and hourly forcast

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,14 +1,38 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import { currentWeatherEndPoint, weatherForecastEndPoint } from './FetchApi'
+import { currentWeatherData } from './Context'
 
 ///COMPONENTS
 
 import Home from './Pages/Home'
 
 
+
 function App() {
+  const [weatherData, setWeatherdata] = useState(null)
+  const [weatherForecast, setWeatherForecast] = useState(null)
+  const API_KEY = import.meta.env.VITE_API_KEY;
+
+  useEffect(() => {
+    Promise.all([
+      fetch(`${currentWeatherEndPoint}?lat=44.34&lon=10.99&units=metric&appid=${API_KEY}`)
+        .then(response => response.json()),
+      fetch(`${weatherForecastEndPoint}?lat=44.34&lon=10.99&units=metric&appid=${API_KEY}`)
+        .then(response => response.json())
+    ]).then(([currentWeatherData, weatherForecastData]) => {
+      setWeatherdata(currentWeatherData);
+      setWeatherForecast(weatherForecastData);
+    }).catch(error => {
+      console.error('Error fetching data:', error);
+    });
+  }, []);
+
   return (
   <>
-    <Home/>
+    <currentWeatherData.Provider value={{weatherData, setWeatherdata, weatherForecast, setWeatherForecast}}>
+      <Home />
+    </currentWeatherData.Provider>
   </>
   )
 }
