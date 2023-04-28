0edea94c15ec6c5e43502f59b37073dfb6d63d61
mohamed-abdelahi-haibelty <96340736+mohamed-abdelahi-haibelty@users.noreply.github.com>
2023-04-28 11:15:46 +0000

    add custom colors to tailwindconfig

--- weather-app/tailwind.config.js
+++ weather-app/tailwind.config.js
@@ -6,6 +6,24 @@ export default {
   ],
   theme: {
     extend: {
+      colors:{
+        blue:{
+          10:'#004BFF',
+          0:`#00B2FF`,
+        },
+        green:{
+          10: ' #4CD964'
+        },
+        yellow:{
+          20:' #FFCC00'
+        },
+        orange:{
+          30:' #FF9500'
+        },
+        red:{
+          40:'#FF2D55 '
+        }
+      },
       fontFamily:{
         heading:['Raleway', 'sans-serif'],
         body:['Montserrat', 'sans-serif']
