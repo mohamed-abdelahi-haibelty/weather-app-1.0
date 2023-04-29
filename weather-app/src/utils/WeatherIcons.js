2ecdfd3c863841fd763d1b5c17511e839b59e25c
mohamed-abdelahi-haibelty <96340736+mohamed-abdelahi-haibelty@users.noreply.github.com>
2023-04-29 14:26:56 +0000

    add weathericon file

---
+++ weather-app/src/utils/WeatherIcons.js
@@ -0,0 +1,25 @@
+import {
+    WiDaySunny,
+    WiDayCloudy,
+    WiCloud,
+    WiCloudy,
+    WiDayShowers,
+    WiDayRain,
+    WiDayThunderstorm,
+    WiDaySnow,
+    WiFog,
+} from "react-icons/wi";
+
+export const weatherIcons = {
+
+    "01": WiDaySunny,
+    "02": WiDayCloudy,
+    "03": WiCloud,
+    "04": WiCloudy,
+    "09": WiDayShowers,
+    "10": WiDayRain,
+    "11": WiDayThunderstorm,
+    "13": WiDaySnow,
+    "50": WiFog,
+
+  };
