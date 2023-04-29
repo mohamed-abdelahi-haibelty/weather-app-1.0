642c161d1537808f77d86f4eca24778a93eacde0
mohamed-abdelahi-haibelty <96340736+mohamed-abdelahi-haibelty@users.noreply.github.com>
2023-04-29 14:25:12 +0000

    add getdays

--- weather-app/src/utils/GetDays.js
+++ weather-app/src/utils/GetDays.js
@@ -1,10 +1,16 @@
 export const getDays = (list) => {
     const days = list.reduce((acc, current) => {
         const day = current.dt_txt.split(" ")[0]
-        if(!acc.includes(day)){
-            acc.push(day)
+
+        if(!acc[0].includes(day)){
+            acc[0].push(day)
+            let icon = current.weather[0].icon.substring(0, 2)
+            acc[1].push(icon)
+            let minmaxTemp = {min: current.main.temp_min, max: current.main.temp_max}
+            acc[2].push(minmaxTemp)
         }
+
         return acc
-    }, [])
+    }, [[],[],[]])
     return days
 }
