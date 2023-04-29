5810fd11d13fd438d1d4907bd07e00339b738dbf
mohamed-abdelahi-haibelty <96340736+mohamed-abdelahi-haibelty@users.noreply.github.com>
2023-04-29 14:26:15 +0000

    add daysForcast component

--- weather-app/src/components/DaysForecast.jsx
+++ weather-app/src/components/DaysForecast.jsx
@@ -1,19 +1,44 @@
-import Temp from "./Temp"
-import { WiRainWind } from "react-icons/wi";
+import DayForcast from "./DayForcast"
+import { useContext, useEffect, useState} from "react"
+import { currentWeatherData } from "../Context"
+import { DateFormat } from "../utils/DateFormat"
+import { getDays } from "../utils/GetDays"
+import { weatherIcons } from "../utils/WeatherIcons"
+
 
 function DaysForecast() {
+  const {weatherForecast} = useContext(currentWeatherData)
+  const [days, setDays] = useState(null)
+  const [icons, setIcons] = useState(null)
+  const [minmaxTemp, setMinmaxTemp] = useState(null)
+  
+  useEffect(() => {
+    if(weatherForecast){
+      let [days, icons, minmaxTemp] = getDays(weatherForecast.list)
+      days = DateFormat(days)
+      const wIcons = icons.map((icon, index) => {
+        const WeatherIcon = weatherIcons[icon]
+        return <WeatherIcon className="w-[25px] h-[25px] text-white" key={index}/>
+      })
+      setDays(days)
+      setIcons(wIcons)
+      setMinmaxTemp(minmaxTemp)
+    }
+  }, [weatherForecast])
 
   return (
     <div className="days-forecast px-4 py-[14px] mt-[8px] card">
             <h3 className="font-body text-[15px] font-medium ml-[15px] mb-[6px] text-white">5-Day FORECAST </h3>
             <div className="days flex flex-col border-t border-white border-opacity-25 ">
-                <div className="day my-[14px] flex justify-between items-center pb-[14px] border-b border-white border-opacity-25">
-                    <span className="date font-body text-[22px] font-medium text-white">Today</span>
-                    <span className="icon"><WiRainWind className="w-[25px] h-[25px] text-white"/></span>
-                    <Temp temperature={'15°'}/>
-                    <div className="progress-bar before:w-[50%] before:left-[30%] before:bg-gradient-to-r before:from-blue-0 before:to-orange-30"></div>
-                    <Temp temperature={'22°'}/>
-                </div>
+              {days && days.map((day, index) => 
+                <DayForcast 
+                  date={day}
+                  min={minmaxTemp[index].min}
+                  max={minmaxTemp[index].max}
+                  icon={icons[index]}
+                  key={index}
+                />
+              )}
             </div>
     </div>
   )
