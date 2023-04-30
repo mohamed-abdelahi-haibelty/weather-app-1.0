a36674c3e2c678cc9e949fd062b032ac75a91e31
mohamed-abdelahi-haibelty <96340736+mohamed-abdelahi-haibelty@users.noreply.github.com>
2023-04-30 14:49:09 +0000

    update fetch api

--- weather-app/src/FetchApi.js
+++ weather-app/src/FetchApi.js
@@ -1,2 +1,3 @@
 export const currentWeatherEndPoint = "https://api.openweathermap.org/data/2.5/weather/"
 export const weatherForecastEndPoint = "https://api.openweathermap.org/data/2.5/forecast/"
+export const geoDbEndPoint = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities"
